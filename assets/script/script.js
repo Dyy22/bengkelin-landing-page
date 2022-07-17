0b52c492a8d178af44d135bdaceaf9f220f754bd
muhammad.perdiansyah <Indivara202221>
2022-07-17 14:02:25 +0700

    initial commit

---
+++ assets/script/script.js
@@ -0,0 +1,31 @@
+window.addEventListener("scroll", function () {
+  let header = document.querySelector("header");
+
+  header.classList.toggle("scrolling", window.scrollY > 0);
+});
+
+let counter = 1;
+setInterval(() => {
+  document.getElementById("radio" + counter).checked = true;
+  counter++;
+  if (counter > 6) counter = 1;
+}, 5000);
+
+let navServices = document.querySelector(".nav-services");
+let servicesList = document.querySelector(".services-list");
+
+navServices.addEventListener("click", () => {
+  servicesList.classList.toggle("show");
+});
+
+let servicesListElements = servicesList.querySelectorAll("li");
+let articleList = document.querySelectorAll("article");
+
+for (let i = 0; i < servicesListElements.length; i++) {
+  servicesListElements[i].addEventListener("click", () => {
+    articleList[i].scrollIntoView({ block: "center" });
+  });
+}
+
+let date = new Date().getFullYear();
+document.querySelector(".year").innerText = date;
